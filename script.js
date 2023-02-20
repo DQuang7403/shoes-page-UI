4d8f152b130434b934578af29d1755ddd371f6e7
Gecko403 <quangnguyen7403@gmail.com>
2023-02-20 21:49:56 +0700

    added toggleBtn function

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload() = function(){
+  main();
+}
+function main(){
+  let toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+  let nav = document.getElementsByClassName("mainNav")[0];
+  toggleBtn.addEventListener('click',()=>{
+    nav.classList.toggle('active');
+  })
+  
+}
